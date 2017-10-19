beb589aa2f6aed25f0d2a87ea0cbb80496a46637
Moises Molina <molina.moisesa@gmail.com>
2017-10-19 20:31:16 +0000

    improved logic

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 var firstApp = angular.module('creative', []);
 firstApp.controller('controlOne', function($scope) {
-  $scope.one = 'First';
+  $scope.one = 'Name';
   $scope.reply = '';
   $scope.answer = '';
   $scope.url = '';
@@ -10,7 +10,7 @@ firstApp.controller('controlOne', function($scope) {
     
   };
   $scope.answerYes = function() {
-      if($scope.one == 'First'){
+      if($scope.one == 'Name'){
           $scope.message = 'Please Enter Your name first!';
       }
       else{
@@ -21,7 +21,7 @@ firstApp.controller('controlOne', function($scope) {
      
   };
     $scope.answerNo = function() {
-    if($scope.one == 'First'){
+    if($scope.one == 'Name'){
         $scope.message = 'Please Enter Your name first!';
     }
     else{
@@ -35,7 +35,11 @@ firstApp.controller('controlOne', function($scope) {
 	var compare = $scope.answer.toUpperCase();
  	if(compare == 'C'){
 		 $scope.finalMessage ='You Win!!';
-		 $scope.show3 = true;
+		 $scope.show2 = false;
+		 $scope.show4 = false;
+	   	 $scope.show5 = false;		
+		 $scope.show = false;
+                 $scope.show3 = true;
 		
 	}
 
@@ -43,15 +47,39 @@ firstApp.controller('controlOne', function($scope) {
 
 		 $scope.finalMessage ='You Loose';
   		$scope.show4 = true;
+		$scope.show3 = false;
+		$scope.show5 = false;
+		$scope.show = false;
+                $scope.show2 = false;
 
 	}
 	else if(compare == 'B'){
-	 $scope.finalMessage ='You Loose';
-	 $scope.show5 = true;
+		 $scope.finalMessage ='You Loose';
+		 $scope.show5 = true;
+		 $scope.show3 = false;
+		 $scope.show4 = false;
+                 $scope.show = false;
+                 $scope.show2 = false;
+
 	}
 	else{
 		$scope.finalMessage ='Please enter A, B, or C';
 	}
 
+	$scope.finalButton = true;
   };
+ $scope.restart = function(){
+		 $scope.finalMessage = ''
+		 $scope.show5 = false;
+                 $scope.show3 = false;
+                 $scope.show4 = false;
+                 $scope.show = true;
+                 $scope.show2 = true;
+		 $scope.finalButton = false;
+	
+	
+
+};
+
+
 });
